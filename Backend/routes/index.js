8c71d754f366295197c5052365066a82a1358c49
chaudharykapil <chaudharykapil8279@gmail.com>
2023-06-09 02:24:21 +0530

    add test url and form

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express();
-require("../DatabaseApi/DB")
+const {TestTable, UserTable} = require("../DatabaseApi/Model")
 var bodyParser = require('body-parser')
 const cors = require('cors');
 router.use(cors({
@@ -10,7 +10,6 @@ router.use(bodyParser.urlencoded({
   extended: true
 }
 ))
-
 // parse application/json
 router.use(bodyParser.json())
 /* GET home page. */
@@ -18,7 +17,23 @@ router.get('/', function(req, res, next) {
   res.send("Hello")
 });
 router.post("/signup",(req,res,next)=>{
-  console.log(req.body)
-  res.send("done")
+  let data = req.body
+  // const t = new TestTable({
+  //   name:req.body.name,
+  //   phone:req.body.phone
+  // });
+  // t.save().then(
+  //       () => console.log("One entry added"), 
+  //       (err) => console.log(err)
+  // );
+  usr = new UserTable({
+    name:data.name,
+    phone:data.phone,
+    email:data.email,
+    dob:data.dob,
+    adhaarid:data.adhaar,
+    address:data.address
+  })
+  usr.save().then(r=>res.status(200).send("success"),err=>res.status(406).send("failed"))
 })
 module.exports = router;
--- slytherins_bloodbank/src/Pages/Test.js
+++ slytherins_bloodbank/src/Pages/Test.js
@@ -5,24 +5,38 @@ class Test extends Component {
         super()
         this.state = {
             name:"",
-            phone:""
+            phone:"",
+            email:"",
+            dob:"",
+            adhaar:"",
+            address:"",
+
+            showmsg:false
         }
     }
     submitform = ()=>{
-        console.log(this.state.name,this.state.phone)
+        let formdata = {
+            "name":this.state.name,
+            "phone":this.state.phone,
+            "email":this.state.email,
+            "dob":this.state.dob,
+            "adhaar": this.state.adhaar,
+            "address":this.state.address
+        }
+        console.log(formdata)
         fetch("http://127.0.0.1:9000/signup",{
             headers:{
                 "Content-Type": "application/json"  
             },
             method:"POST",
-            body:JSON.stringify({
-                name:this.state.name,
-                phone:this.state.phone
-            })
+            body:JSON.stringify(formdata)
         })
         .then(
             res=>{
                 console.log(res.status)
+                if(res.status == 200){
+                    this.setState({showmsg:true})
+                }
             },
             err=>{
                 console.error(err)
@@ -31,17 +45,32 @@ class Test extends Component {
     }
     render() {
         return (
-            <div className='bg-red-500'>
+            <div>
                 <h1>Test page</h1>
                 <div className='justify-center item-center'>
                     <form method='POST' action='http://127.0.0.1:9000/signup'>
-                    <input type={"text"} name="name" onChange={val=>this.setState({name:val.target.value})} />
+                    <input type={"text"} name="name" placeholder='name' onChange={val=>this.setState({name:val.target.value})} />
                     <br />
-                    <input type={"tel"} name="phone" onChange={val=>this.setState({phone:val.target.value})} />
+                    <input type = {"email"} name = "email" placeholder='email' onChange={val=>this.setState({email:val.target.value})}  />
+                    <br/>
+                    <input type={"date"} name="dob" placeholder='dob' onChange={val=>this.setState({dob:val.target.value})}  />
+                    <br/>
+                    <input type={"number"} name="adhaar" placeholder='adhaar' onChange={val=>this.setState({adhaar:val.target.value})}  />
+                    <br/>
+                    <input type={"tel"} name="phone" placeholder='phone' onChange={val=>this.setState({phone:val.target.value})}  />
+                    <br/>
+                    <textarea name='address' placeholder='address' onChange={val=>this.setState({address:val.target.value})} />
+                    <br/>
                     <button type='button' onClick={this.submitform}  >click</button>
                     </form>
 
                 </div>
+                {this.state.showmsg?
+                <h1>Added successfully</h1>
+                :
+                null
+                }
+
             </div>
         );
     }
